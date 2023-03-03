920d0760cde6e09fb068c03b7c42842439e0d155
FireController#1847 <usfirepilot123@gmail.com>
2023-03-02 17:56:18 -0700

    Add content

--- site.js
+++ site.js
@@ -1,35 +1,44 @@
-const sectionMain = document.getElementById("main");
-const sectionGoal = document.getElementById("goal");
-const sectionMission = document.getElementById("mission");
-const sectionContact = document.getElementById("contact");
-const sectionMainBoundingRect = sectionMain.getBoundingClientRect();
-const sectionGoalBoundingRect = sectionGoal.getBoundingClientRect();
-const sectionMissionBoundingRect = sectionMission.getBoundingClientRect();
-const sectionContactBoundingRect = sectionContact.getBoundingClientRect();
+window.addEventListener("load", () => {
+    const sectionMain = document.getElementById("main");
+    const sectionGoal = document.getElementById("goal");
+    const sectionMission = document.getElementById("mission");
+    const sectionContact = document.getElementById("contact");
+    const sectionMainBoundingRect = sectionMain.getBoundingClientRect();
+    const sectionGoalBoundingRect = sectionGoal.getBoundingClientRect();
+    const sectionMissionBoundingRect = sectionMission.getBoundingClientRect();
+    const sectionContactBoundingRect = sectionContact.getBoundingClientRect();
 
-window.addEventListener("scroll", function(args) {
-    const bodyRect = document.body.getBoundingClientRect();
-    const negY = -bodyRect.y;
-    console.log("NEGY: " + negY);
-    if (negY <= sectionMainBoundingRect.bottom - 20) {
-        if (window.location.hash != "") {
-            console.log("MAIN");
-            history.replaceState("", document.title, window.location.pathname + window.location.search);
+    let dateOfButton = 0;
+    let scrollDebounce = 2000;
+
+    window.addEventListener("click", function(args) {
+        if (args.target != null && (args.target.nodeName == "A" || args.target.nodeName == "BUTTON")) {
+            dateOfButton = Date.now();
         }
-    } else if (negY <= sectionGoalBoundingRect.bottom) {
-        if (window.location.hash != "#goal") {
-            console.log("GOAL");
-            history.replaceState(null, null, "#goal");
+    });
+
+    window.addEventListener("scroll", function(args) {
+        if (Date.now() - dateOfButton < scrollDebounce) {
+            return;
         }
-    } else if (negY <= sectionMissionBoundingRect.bottom) {
-        if (window.location.hash != "#mission") {
-            console.log("MISSION");
-            history.replaceState(null, null, "#mission");
+        const bodyRect = document.body.getBoundingClientRect();
+        const negY = -bodyRect.y;
+        if (negY <= sectionMainBoundingRect.bottom) {
+            if (window.location.hash != "") {
+                history.replaceState("", document.title, window.location.pathname + window.location.search);
+            }
+        } else if (negY <= sectionGoalBoundingRect.bottom) {
+            if (window.location.hash != "#goal") {
+                history.replaceState(null, null, "#goal");
+            }
+        } else if (negY <= sectionMissionBoundingRect.bottom) {
+            if (window.location.hash != "#mission") {
+                history.replaceState(null, null, "#mission");
+            }
+        } else if (negY <= sectionContactBoundingRect.bottom) {
+            if (window.location.hash != "#contact") {
+                history.replaceState(null, null, "#contact");
+            }
         }
-    } else if (negY <= sectionContactBoundingRect.bottom - 20) {
-        if (window.location.hash != "#contact") {
-            console.log("CONTACT");
-            history.replaceState(null, null, "#contact");
-        }
-    }
+    });
 });
